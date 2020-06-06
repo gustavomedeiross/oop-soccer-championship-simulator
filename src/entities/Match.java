9c3e628fa537f825f1674476925becadf32b8681
Gustavo <gustavomendes.medeiros@gmail.com>
2020-06-06 17:18:10 -0300

    Develop match logic

--- src/entities/Match.java
+++ src/entities/Match.java
@@ -3,47 +3,60 @@ package entities;
 public class Match {
     private Team team1;
     private Team team2;
-    private Team winner;
+
+    private static final int TOTAL_MATCH_MINUTES = 90;
+    private static final int AVERAGE_MOVE_TIME = 2;
+    private static final int TOTAL_MATCH_MOVES = TOTAL_MATCH_MINUTES / AVERAGE_MOVE_TIME;
 
     public Match(Team team1, Team team2) {
         this.team1 = team1;
         this.team2 = team2;
     }
 
-    public Team getTeam1() {
-        return team1;
-    }
-
-    public void setTeam1(Team team1) {
-        this.team1 = team1;
-    }
-
-    public Team getTeam2() {
-        return team2;
-    }
-
-    public void setTeam2(Team team2) {
-        this.team2 = team2;
-    }
-
     public Team getWinner() {
-        return winner;
+        int team1Goals = team1.getGoals();
+        int team2Goals = team2.getGoals();
+        if (team1Goals > team2Goals) {
+            return team1;
+        } else if (team2Goals > team1Goals) {
+            return team2;
+        } else {
+            return null;
+        }
     }
 
     public void start() {
-        winner = null;
+        Team currentAttackingTeam;
+        Team currentDefendingTeam;
+
+        // Basicamente eu estou considerando que cada time vai fazer uma jogada de cada vez
+        for (int i = 0; i < TOTAL_MATCH_MOVES; i++) {
+            if (i % 2 == 0) {
+                currentAttackingTeam = team1;
+                currentDefendingTeam = team2;
+            } else {
+                currentAttackingTeam = team2;
+                currentDefendingTeam = team1;
+            }
+
+            double oddsOfScoringAGoal = getCurrentAttackingTeamOddsOfScoringAGoal(currentAttackingTeam, currentDefendingTeam);
+            double randomNumber = Math.random() * 100;
+            if (oddsOfScoringAGoal > randomNumber) {
+                currentAttackingTeam.scoreGoal();
+            }
+        }
     }
 
-    private void calculateOddsOfWinning() {
-        int team1Skill = team1.getTotalTeamSkill();
-        int team2Skill = team2.getTotalTeamSkill();
+    private double getCurrentAttackingTeamOddsOfScoringAGoal(Team attacker, Team defender) {
+        double attackerSkills = attacker.getTotalAttackingSkills();
+        double defenderSkills = defender.getTotalDefensiveSkills();
 
-        int totalTeamsSkill = team1Skill + team2Skill;
-        int team1OddsOfWinning = this.calculateSingleTeamOddOfWinning(team1Skill, totalTeamsSkill);
-        int team2OddsOfWinnig = this.calculateSingleTeamOddOfWinning(team1Skill, totalTeamsSkill);
-    }
+        double total = attackerSkills + defenderSkills;
 
-    private int calculateSingleTeamOddOfWinning(int teamSkill, int bothTeamsTotalSkill) {
-        return teamSkill * 100 / bothTeamsTotalSkill;
+        /**
+         * ainda estou divindo o resultado final por 3 porque a chance de se fazer um gol já é naturalmente baixa em um jogo de futebol,
+         * mesmo que um time seja muito melhor que o outro, provavelmente o gol não irá acontecer mais que 5 vezes em uma partida
+         */
+        return (attackerSkills * 100 / total) / 3;
     }
 }
--- src/entities/Team.java
+++ src/entities/Team.java
@@ -1,11 +1,16 @@
 package entities;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Team {
     private String name;
-    private List<Player> players;
-    private int goals;
+    private List<GoalkeeperPlayer> goalkeeperPlayers;
+    private List<DefenderPlayer> defenderPlayers;
+    private List<MidfielderPlayer> midfielderPlayers;
+    private List<ForwardPlayer> forwardPlayers;
 
     public Team(String name) {
         this.name = name;
@@ -20,32 +25,69 @@ public class Team {
     }
 
     public List<Player> getPlayers() {
-        return players;
+        return Stream.of(goalkeeperPlayers, defenderPlayers, midfielderPlayers, forwardPlayers).flatMap(Collection::stream).collect(Collectors.toList());
     }
 
-    public void addPlayer(Player player) {
-        this.players.add(player);
+    public void addPlayer(GoalkeeperPlayer player) {
+        goalkeeperPlayers.add(player);
     }
 
-    public void setPlayers(List<Player> players) {
-        this.players = players;
+    public void addPlayer(DefenderPlayer player) {
+        defenderPlayers.add(player);
     }
 
-    public int getGoals() {
-        return goals;
+    public void addPlayer(MidfielderPlayer player) {
+        midfielderPlayers.add(player);
     }
 
-    public void setGoals(int goals) {
-        this.goals = goals;
+    public void addPlayer(ForwardPlayer player) {
+        forwardPlayers.add(player);
     }
 
-    public double getTotalTeamSkill() {
+    int getGoals() {
+        int total = 0;
+
+        for (Player player: getPlayers()) {
+            total += player.getGoals();
+        }
+
+        return total;
+    }
+
+    double getTotalAttackingSkills() {
         double sum = 0;
 
-        for (Player player: players) {
+        for (ForwardPlayer player: forwardPlayers) {
             sum += player.getSkill();
         }
 
+        for (MidfielderPlayer player: midfielderPlayers) {
+            sum += player.getSkill() / 2;
+        }
+
         return sum;
+    }
+
+    double getTotalDefensiveSkills() {
+        double sum = 0;
+
+        for (GoalkeeperPlayer player: goalkeeperPlayers) {
+            sum += player.getSkill();
+        }
+
+        for (DefenderPlayer player: defenderPlayers) {
+            sum += player.getSkill();
+        }
+
+        for (MidfielderPlayer player: midfielderPlayers) {
+            sum += player.getSkill() / 2;
+        }
+
+        return sum;
+    }
+
+    void scoreGoal() {
+        int index = (int) Math.floor(Math.random() * forwardPlayers.size());
+        forwardPlayers.get(index).scoreGoal();
     }
 }
